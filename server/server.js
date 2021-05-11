96c18dc0a9e9da85d14ca2d1b6aa17945897be24
VirtCoder <joekic@gmail.com>
2021-05-10 20:07:37 -0400

    done with listing description

--- server/server.js
+++ server/server.js
@@ -8,20 +8,10 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/listingheader', (req, res) => {
-  models.listings.get((err, results)=> {
-    res.json(results);
-  });
-});
-
-app.get('/api/listingheader/:id', (req, res) => {
+app.get('/api/listingdescription/:id', (req, res) => {
   models.listing.get(req.params.id, (err, result) => {
     res.json(result);
   });
-});
-
-app.post('/', (req, res) => {
-
 });
 
 app.listen(port, () => {
