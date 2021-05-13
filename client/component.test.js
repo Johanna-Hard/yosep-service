c744efc44525a876a2ab61d2c7c8862d5c7522fa
VirtCoder <joekic@gmail.com>
2021-05-13 18:24:05 -0400

    added unit test

--- client/component.test.js
+++ client/component.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import App from './src/index';
+import { configure, shallow, mount } from 'enzyme';
+import App from './src/App';
+import Adapter from 'enzyme-adapter-react-16';
+import 'regenerator-runtime/runtime';
 
-describe("Rendering components", () => {
+configure({ adapter: new Adapter() });
+
+describe("Rendering App components", () => {
   it("renders App component without crashing", () => {
     shallow(<App />);
   });
+
+  test("renders App component hostedBy without crashing", async () => {
+    const wrapper = await mount(<App />);
+    const content = (<span id='forTest'></span>);
+    expect(wrapper.contains(content)).toEqual(true);
+  });
 });
+
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,13 +23,12 @@ class App extends React.Component {
 
   getData() {
     // console.log(this.state.listingId);
-    // fetch(`/api/hostedBy/${this.state.listingId}`)
     axios.get(`/api/hostedBy/${this.state.listingId}`)
       .then(resp => {
-        this.setState({ listing: resp.data[0] });
+        this.setState({ listing: resp.data });
       })
       .catch(err => {
-        console.log('===== AXIOS get request failed');
+        // console.log('===== AXIOS get request failed');
       });
   }
 
@@ -41,6 +40,7 @@ class App extends React.Component {
     return (
       <div className="hostedBy">
         {parse(this.state.listing.accomodates + ' guest' + this.isPlural(this.state.listing.accomodates) + ' ' + unescape('&middot;') + ' ' + this.state.listing.bedrooms + ' bedroom' + this.isPlural(this.state.listing.bedrooms) + ' ' + unescape('&middot;') + ' ' + this.state.listing.beds + ' bed' + this.isPlural(this.state.listing.beds) + ' ' + unescape('&middot;') + ' ' + this.state.listing.bathrooms + ' bath' + this.isPlural(this.state.listing.bathrooms))}
+        <span id='forTest'></span>
       </div>
     )
   }
