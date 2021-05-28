466942c93ac70405d1df54ec6dc23378891b6157
VirtCoder <joekic@gmail.com>
2021-05-27 23:10:54 -0400

    moved code from hosted_by and renamed some files

--- server/server.js
+++ server/server.js
@@ -5,15 +5,17 @@ const models = require('./models');
 
 const app = express();
 app.use(cors());
-const port = 5003;
+const port = 5001;
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/api/hostedBy/:id', (req, res) => {
   models.hostedBy.get(req.params.id, (err, result) => {
     if (err) {
       console.log('Error retrieving hostedBy info');
+      res.end();
     } else {
       console.log(result[0]);
       res.json(result[0]);
